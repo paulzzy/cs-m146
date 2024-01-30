81c0a8ea59e82cf714d4269054787427cd931db1
Paul Zhang <paulz@ucla.edu>
2024-01-29 18:43:27 -0800

    HW1: `ruff format`, `ruff check --fix .`, disable Google Drive imports

--- hw1/nutil.py
+++ hw1/nutil.py
@@ -4,14 +4,12 @@ Description : ML utilities
 """
 
 # python libraries
-import os
 
 # numpy libraries
 import numpy as np
 
 # matplotlib libraries
 import matplotlib as mpl
-import matplotlib.pyplot as plt
 
 ######################################################################
 # global settings
@@ -25,58 +23,57 @@ mpl.axes.labelsize = 14
 # classes
 ######################################################################
 
-class Data :
-    
-    def __init__(self) :
+
+class Data:
+    def __init__(self):
         """
         Data class.
-        
+
         Attributes
         --------------------
             X -- numpy array of shape (n,d), features
             y -- numpy array of shape (n,), targets
         """
-                
+
         # n = number of examples, d = dimensionality
         self.X = None
         self.y = None
-        
+
         self.Xnames = None
         self.yname = None
-    
-    def load(self, f, header=0, predict_col=-1) :
+
+    def load(self, f, header=0, predict_col=-1):
         """Load csv file into X array of features and y array of labels."""
 
-        
         # load data
-        with open(f, 'r') as fid :
+        with open(f, "r") as fid:
             data = np.loadtxt(fid, delimiter=",", skiprows=header)
-        
+
         # separate features and labels
-        if predict_col is None :
-            self.X = data[:,:]
+        if predict_col is None:
+            self.X = data[:, :]
             self.y = None
-        else :
-            if data.ndim > 1 :
+        else:
+            if data.ndim > 1:
                 self.X = np.delete(data, predict_col, axis=1)
-                self.y = data[:,predict_col]
-            else :
+                self.y = data[:, predict_col]
+            else:
                 self.X = None
                 self.y = data[:]
-        
+
         # load feature and label names
         if header != 0:
-            with open(f, 'r') as fid :
+            with open(f, "r") as fid:
                 header = fid.readline().rstrip().split(",")
-                
-            if predict_col is None :
+
+            if predict_col is None:
                 self.Xnames = header[:]
                 self.yname = None
-            else :
-                if len(header) > 1 :
+            else:
+                if len(header) > 1:
                     self.Xnames = np.delete(header, predict_col)
                     self.yname = header[predict_col]
-                else :
+                else:
                     self.Xnames = None
                     self.yname = header[0]
         else:
@@ -85,7 +82,7 @@ class Data :
 
 
 # helper functions
-def load_data(filename, header=0, predict_col=-1) :
+def load_data(filename, header=0, predict_col=-1):
     """Load csv file into Data class."""
     data = Data()
     data.load(filename, header=header, predict_col=predict_col)
